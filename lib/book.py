23914ee84267393c1592a53cd0ce21b0028df530
Mary <marshareeitumo@gmail.com>
2025-08-08 10:42:57 +0300

    adds lib

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,42 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, author=None, page_count=272, genre=None):
+        self.title = title
+        self._author = author
+        self._page_count = page_count
+        self._genre = genre
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, author):
+        self._author = author
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, page_count):
+        if isinstance(page_count, int):
+            self._page_count = page_count
+        else:
+            print("page_count must be an integer")
+
+    @property
+    def genre(self):
+        return self._genre
+
+    @genre.setter
+    def genre(self, genre):
+        self._genre = genre
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,41 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size=0):
+        self.brand = brand
+        self._size = size
+        self._color = None
+        self._material = None
+        self.condition = "Used"
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    @property
+    def color(self):
+        return self._color
+
+    @color.setter
+    def color(self, color):
+        self._color = color
+
+    @property
+    def material(self):
+        return self._material
+
+    @material.setter
+    def material(self, material):
+        self._material = material
+
